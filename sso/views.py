947a7f9c00fb50ed16f26bca0b4cde3c9a1cb998
Sheng Wu <pkernoobie@gmail.com>
2014-07-21 18:14:55 -0500

    charmingly good looks

--- sso/views.py
+++ sso/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Log this user into all of ASG's services
 def sso_login(request):
+    if 'sso_redirect' not in request.GET:
+        return redirect('https://asg.northwestern.edu')
+
     return render(request, 'login.html', locals())
