14739d4721f936121fa721246c71dc4bdaa6dc99
Sheng Wu <pkernoobie@gmail.com>
2014-08-19 21:07:13 -0500

    use root URL as login and ASG template

--- sso/urls.py
+++ sso/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from mama_cas.views import LoginView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,7 +9,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'sso.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'sso.views.sso_login', name='sso_login'),
+    #url(r'^$', 'sso.views.sso_login', name='sso_login'),
+    url(r'^$', LoginView.as_view()),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('mama_cas.urls')),
